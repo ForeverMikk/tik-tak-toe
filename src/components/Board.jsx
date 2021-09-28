b1653681c72b167098b025f4044ae151df6b8923
ForeverMikk <michaelmolina679@gmail.com>
2021-09-28 16:16:53 -0500

    Showing board numbers

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -5,7 +5,7 @@ import Square from './Square'
 const Board = () => {
 
     function renderSquare(i){
-        return <Square />
+        return <Square value={i}/>
     }
     
     const status = 'Next Player: X'
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Square = () => {
+const Square = (props) => {
     return(
         <button className="square">
-            { /* TODO */}
+            {props.value}
         </button>
     )
 }
